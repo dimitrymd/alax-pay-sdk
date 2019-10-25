26b831b85fcaf3c7c1d4580b86494829bcd129bd
Casparova Olga <galuncaolga@gmail.com>
2019-10-25 12:14:15 +0300

    CallHelper class added
    CallHelper is a unique entry point into the lib features from other non-native environments.

--- library/src/main/java/io/alax/sdk/pay/model/Asset.kt
+++ library/src/main/java/io/alax/sdk/pay/model/Asset.kt
@@ -4,10 +4,10 @@ import java.math.BigDecimal
 
 enum class Asset(val symbol: String, val minAmountToPay: BigDecimal) {
   AIA("AIA", BigDecimal("0.02")),
-  ALAT(symbol: "ALAT", BigDecimal("0.02")),
+  ALAT("ALAT", BigDecimal("0.02")),
   ALX("ALX", BigDecimal("0.000002"));
 
   companion object {
     val DEFAULT = ALAT
   }
-}
+}+
